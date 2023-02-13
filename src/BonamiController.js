7ac32c26f1c5815fe96679850d5309d4b967b28f
UnionIM <dam.rowv@gmail.com>
2023-02-13 14:32:21 +0200

    Login,getUserData, updateUserData,createItem methods

--- src/BonamiController.js
+++ src/BonamiController.js
@@ -38,17 +38,95 @@ class BonamiController {
     }
   }
 
-  async getUserData(req, res) {
+  async login(req, res) {
     try {
-      const { email } = req.body;
-      if (!email) {
+      const { email, password } = req.body;
+      if (!email || !password) {
         res.status(400);
         return res.json({
-          error: 'Enter a email',
+          error: 'Enter a email or password',
         });
       }
-      const user = await BonamiService.getUserData(email);
+      const user = await BonamiService.login(email, password);
       res.json(user);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async getUserData(req, res) {
+    try {
+      const _id = req.user._id;
+      if (!_id) {
+        res.status(400);
+        return res.json({
+          error: 'Enter a id',
+        });
+      }
+      const user = await BonamiService.getUserData(_id);
+      res.json(user);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async updateUserData(req, res) {
+    try {
+      const _id = req.user._id;
+      if (!_id) {
+        res.status(400);
+        return res.json({
+          error: 'Enter a id',
+        });
+      }
+      const { email, password, phone, socialMedia, firstName, secondName } =
+        req.body;
+      if (!phone && !socialMedia) {
+        res.status(400);
+        return res.json({
+          error: 'Enter a phone number or at least 1 social media id or tag',
+        });
+      }
+      const user = await BonamiService.updateUserData(
+        _id,
+        email,
+        password,
+        phone,
+        socialMedia,
+        firstName,
+        secondName
+      );
+      res.json(user);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async createItem(req, res) {
+    try {
+      const {
+        nameEn,
+        nameUa,
+        descriptionEn,
+        descriptionUa,
+        price,
+        discount,
+        categoryEn,
+        categoryUa,
+      } = req.body;
+      const files = req.files;
+      const item = await BonamiService.createItem(
+        nameEn,
+        nameUa,
+        descriptionEn,
+        descriptionUa,
+        price,
+        discount,
+        categoryEn,
+        categoryUa,
+        files
+      );
+      res.json(item);
     } catch (e) {
       res.status(500).json(e.message);
     }
--- src/BonamiService.js
+++ src/BonamiService.js
@@ -1,5 +1,9 @@
 import bcrypt from 'bcrypt';
 import User from '../db/models/User.js';
+import passport from 'passport';
+import Item from '../db/models/Item.js';
+import mongoose from 'mongoose';
+import { s3Uploadv2 } from './s3service.js';
 
 class BonamiService {
   async SignUpUser(email, password, phone, socialMedia, firstName, secondName) {
@@ -19,11 +23,81 @@ class BonamiService {
       createdAt: Date.now(),
       updatedAt: Date.now(),
     });
-
     return user;
   }
-  async getUserData(email) {
-    return User.findOne({ email: email }).exec();
+
+  async login(email, password) {
+    const user = await User.findOne({ email: email }).exec();
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return 'Wrong email or password';
+    }
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+    return user;
+  }
+
+  async getUserData(_id) {
+    return User.findOne({ _id: _id }).exec();
+  }
+
+  async updateUserData(
+    _id,
+    email,
+    password,
+    phone,
+    socialMedia,
+    firstName,
+    secondName
+  ) {
+    return User.updateOne(
+      { _id },
+      {
+        email,
+        password,
+        phone,
+        socialMedia,
+        firstName,
+        secondName,
+        updatedAt: Date.now(),
+      }
+    );
+  }
+
+  async createItem(
+    nameEn,
+    nameUa,
+    descriptionEn,
+    descriptionUa,
+    price,
+    discount,
+    categoryEn,
+    categoryUa,
+    files
+  ) {
+    const id = new mongoose.Types.ObjectId();
+    const results = await s3Uploadv2(files, id);
+    const imgArray = results.map((el) => {
+      return { url: el.Location };
+    });
+    const item = await Item.create({
+      name: {
+        en: nameEn,
+        ua: nameUa,
+      },
+      description: {
+        en: descriptionEn,
+        ua: descriptionUa,
+      },
+      price,
+      discount,
+      images: imgArray,
+      category: {
+        en: categoryEn,
+        ua: categoryUa,
+      },
+    });
+    return item;
   }
 }
 
