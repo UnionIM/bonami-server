54ba2364a4466d0301f682e72ae0f62b660e1de8
UnionIM <dam.rowv@gmail.com>
2023-02-20 12:36:30 +0200

    Cors and cookie session

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 import express from 'express';
 import router from './src/router.js';
 import mongoose from 'mongoose';
-import session from 'express-session';
+import cookieSession from 'cookie-session';
 import * as dotenv from 'dotenv';
 import passport from 'passport';
+import cors from 'cors';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(session({ secret: `${process.env.SESSION_SECRET}` }));
+app.use(
+  cookieSession({
+    name: 'sessionA',
+    keys: ['cat'],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(
+  cors({
+    origin: process.env.ADMIN_PANEL_URL,
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use('/', router);
