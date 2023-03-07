92f4a54100517a93fdc9c5e3b69a088ab5f182ee
UnionIM <dam.rowv@gmail.com>
2023-03-07 17:07:14 +0200

    Order model

--- db/models/Order.js
+++ db/models/Order.js
@@ -1,8 +1,33 @@
 import mongoose from 'mongoose';
 
-export const Order = new mongoose.Schema({
-  itemId: String,
-  amount: Number,
-  userId: String,
+export const OrderSchema = new mongoose.Schema({
+  items: [{ id: String, name: String, price: Number, amount: Number }],
+  email: String,
+  phoneNumber: String,
+  socialMedia: {
+    telegram: String,
+    instagram: String,
+    facebook: String,
+    viber: String,
+  },
+  delivery: {
+    country: String,
+    city: String,
+    street: String,
+    address: String,
+    deliveryCompanyName: String,
+    postOfficeNumber: String,
+  },
+  name: {
+    firstName: String,
+    secondName: String,
+    patronymic: String,
+  },
+  status: String,
+  isAuthenticated: Boolean,
   createdAt: Date,
 });
+
+const Order = mongoose.model('Order', OrderSchema);
+
+export default Order;
